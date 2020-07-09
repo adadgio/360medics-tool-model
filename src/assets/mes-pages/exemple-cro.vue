9f69c7f6d444cd9ef0e52dce8bd3d8cd2f669f07
Kal0u <rome.bastien@gmail.com>
2020-07-09 11:36:52 +0200

    update json & add some components

---
+++ src/assets/mes-pages/exemple-cro.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Je suis le titre du compte rendu opératoire n° 1</h1>
+      <a href="https://www.google.com">Je suis un lien utile (ou pas)</a>
+      <br />
+      <p>
+        Suspendisse potenti. Sed vitae interdum sapien, imperdiet feugiat enim.
+        Mauris varius a nisi non ultricies. Etiam non suscipit odio. Aliquam
+        erat volutpat. Nam non lacinia orci. Fusce a mauris posuere, faucibus
+        ligula id, efficitur ligula. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Aliquam non malesuada diam, a scelerisque nunc.
+        Praesent sit amet ultricies ante, quis sollicitudin tortor. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Suspendisse at cursus arcu. Donec dictum enim at sem porttitor, in
+        volutpat turpis lacinia. Curabitur sed dapibus tellus, ut euismod
+        lectus.
+      </p>
+    </div>
+</template>
---
+++ src/assets/mes-pages/exemple-cro2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Je suis le titre du compte rendu opératoire n° 2</h1>
+    <a href="https://www.google.com">Je suis un lien utile (ou pas)</a>
+    <br />
+    <p>
+      Donec efficitur pellentesque efficitur. Nunc metus quam, tempor eu nisl
+      non, vestibulum posuere purus. Maecenas mi mauris, finibus nec sodales
+      sed, dignissim et elit. Ut turpis urna, placerat ac auctor vel, semper ac
+      nisi. Mauris at elit interdum, rhoncus ipsum ut, viverra nulla. Integer
+      efficitur efficitur turpis eu rutrum. Donec enim purus, elementum ut magna
+      malesuada, laoreet dictum odio. Aenean augue turpis, finibus et
+      pellentesque ut, venenatis vel ligula. In volutpat consequat eleifend.
+      Praesent vel nunc sit amet nisi fringilla feugiat. Etiam sit amet dapibus
+      neque. In velit felis, facilisis nec rutrum eu, fermentum a lorem.
+    </p>
+  </div>
+</template>
