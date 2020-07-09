5c0961afd7df75fa225922c8487ac7c64a2784af
Kal0u <rome.bastien@gmail.com>
2020-07-09 12:15:00 +0200

    add custom content

--- src/assets/mes-pages/page-1-2.vue
+++ src/assets/mes-pages/page-1-2.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-<h1>I'm Title 2 </h1>
-<a href="www.google.com">I'm a link number 2</a>    
-<p>I'm a paragraph number 2</p><br>
-<p>Aliquam tristique nibh eget vehicula pretium. In sodales dolor lorem, in euismod nunc varius eu. Vivamus viverra purus eleifend, pretium massa non, ultricies nisl. Nunc mollis at lacus auctor rhoncus. Mauris hendrerit vulputate dui, vel iaculis nisl interdum eget. Aliquam ullamcorper sagittis lorem ac vehicula. Pellentesque consectetur mi non enim commodo luctus. Cras ac interdum dolor, vitae elementum elit. Pellentesque sit amet neque vel odio ornare lacinia at nec lacus. </p>
+    <h1>I'm Title 2</h1>
+    <a href="www.google.com">I'm a link number 2</a>
+    <p>I'm a paragraph number 2</p>
+    <br />
+    <p>
+      Aliquam tristique nibh eget vehicula pretium. In sodales dolor lorem, in
+      euismod nunc varius eu. Vivamus viverra purus eleifend, pretium massa non,
+      ultricies nisl. Nunc mollis at lacus auctor rhoncus. Mauris hendrerit
+      vulputate dui, vel iaculis nisl interdum eget. Aliquam ullamcorper
+      sagittis lorem ac vehicula. Pellentesque consectetur mi non enim commodo
+      luctus. Cras ac interdum dolor, vitae elementum elit. Pellentesque sit
+      amet neque vel odio ornare lacinia at nec lacus.
+    </p>
   </div>
 </template>
